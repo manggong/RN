6758fec05074a5f26182ad75d027c9609397999c
manggong <jihwank1995@gmail.com>
2020-04-20 23:08:06 +0900

    Add styled-components

--- firstapp/App.tsx
+++ firstapp/App.tsx
@@ -7,14 +7,7 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
 
 import {
   Header,
@@ -24,6 +17,20 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import Styled from 'styled-components/native';
+
+const ScrollView = Styled.ScrollView`
+  background-color: ${Colors.lighter};
+`;
+
+const View = Styled.View`background-color: ${Colors.white}`;
+
+const Text = Styled.Text`
+margin-top: 8px; 
+font-size: 18px; 
+font-weight: 400; 
+color: ${Colors.dark}`;
+
 interface Props {}
 
 const App = ({}: Props) => {
@@ -31,40 +38,36 @@ const App = ({}: Props) => {
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
           <Header />
           {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+            <View>
+              <Text>Engine: Hermes</Text>
             </View>
           )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
+          <View>
+            <View>
+              <Text>Step One</Text>
+              <Text>
+                Edit <Text>App.js</Text> to change this screen and then come
+                back to see your edits.
               </Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
+            <View>
+              <Text>See Your Changes</Text>
+              <Text>
                 <ReloadInstructions />
               </Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
+            <View>
+              <Text>Debug</Text>
+              <Text>
                 <DebugInstructions />
               </Text>
             </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
+            <View>
+              <Text>Learn More</Text>
+              <Text>Read the docs to discover what to do next:</Text>
             </View>
             <LearnMoreLinks />
           </View>
@@ -73,44 +76,5 @@ const App = ({}: Props) => {
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
